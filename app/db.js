07f81bd30b4b794c7e1abe426670ed310863e9e7
sharath181 <sharathchandra181@gmail.com>
2022-01-18 04:09:40 -0600

    Update db.js

--- app/db.js
+++ app/db.js
@@ -1,11 +1,18 @@
-const Pool = require("pg").Pool;
+// const Pool = require("pg").Pool;
 
+// const pool = new Pool({
+//     user: "cehntnmesclxvn",
+//     password: "aabf93990eeb525d84c2ee2e9f8f9d9dc9dd3e99214ba14d0356698c8f8b3b06",
+//     host: "ec2-52-71-161-140.compute-1.amazonaws.com",
+//     port: 5432,
+//     database: "d5l097skrv9563"
+// });
+
+const { Pool } = require('pg');
 const pool = new Pool({
-    user: "cehntnmesclxvn",
-    password: "aabf93990eeb525d84c2ee2e9f8f9d9dc9dd3e99214ba14d0356698c8f8b3b06",
-    host: "ec2-52-71-161-140.compute-1.amazonaws.com",
-    port: 5432,
-    database: "d5l097skrv9563"
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
-
-module.exports = pool;
+ module.exports = pool;
