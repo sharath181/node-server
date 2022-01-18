31bf937cad1a1ef9951b5249c344e9bd982d629e
sharath181 <sharathchandra181@gmail.com>
2022-01-18 04:17:05 -0600

    Update index.js

--- app/index.js
+++ app/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = 7210;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
     console.log("Server is running on " + PORT);
