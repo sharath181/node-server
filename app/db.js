ba40a9421a67eef4a576a56c324dfb3204859058
sharath181 <sharathchandra181@gmail.com>
2022-01-18 04:05:04 -0600

    Update db.js

--- app/db.js
+++ app/db.js
@@ -1,11 +1,11 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    user: "todo_dev",
-    password: "password",
-    host: "localhost",
+    user: "cehntnmesclxvn",
+    password: "aabf93990eeb525d84c2ee2e9f8f9d9dc9dd3e99214ba14d0356698c8f8b3b06",
+    host: "ec2-52-71-161-140.compute-1.amazonaws.com",
     port: 5432,
-    database: "todo_db"
+    database: "d5l097skrv9563"
 });
 
 module.exports = pool;
